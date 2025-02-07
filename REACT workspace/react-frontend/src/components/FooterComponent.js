a805ac00e36a23d728e64e027954343d2de8908a
Nani K <manishnane@gmail.com>
2025-02-07 09:35:46 +0530

    react work space

---
+++ REACT workspace/react-frontend/src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+function FooterComponent() {
+  return (
+        <footer className='footer'>
+            <small>All Right Reserved &copy; by Niyaz Sir</small>
+        </footer>
+  )
+}
+
+export default FooterComponent
+
